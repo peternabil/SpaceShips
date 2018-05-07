278e9088b725803945db89b5c3ff984f6a270de0
Seif AbdelHakim <37963932+seiouf97@users.noreply.github.com>
2018-05-08 00:57:14 +0200

    Add files via upload

---
+++ src/GAME/StarWarsMain.java
@@ -0,0 +1,15 @@
+package star.wars.main;
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class StarWarsMain {
+
+    public static void main(String[] args) {
+        StarWarsMainPage s = new StarWarsMainPage();
+        s.setVisible(true);
+    }
+    
+}
