6796a587ed87b8ced58fcd2c16879f5ce27d1396
Peter Nabil <p.nabil@icloud.com>
2018-10-04 23:57:38 +0200

    Update StarWarsMain.java

--- src/GAME/StarWarsMain.java
+++ src/GAME/StarWarsMain.java
@@ -1,15 +1,15 @@
-//package GAME;
-//
-//import javax.swing.*;
-//import java.util.*;
-//import java.awt.*;
-//import java.awt.event.*;
-//
-//public class StarWarsMain {
-//
-//    public static void main(String[] args) {
-//        StarWarsMainPage s = new StarWarsMainPage();
-//        s.setVisible(true);
-//    }
-//
-//}
+package GAME;
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class StarWarsMain {
+
+   public static void main(String[] args) {
+       StarWarsMainPage s = new StarWarsMainPage();
+       s.setVisible(true);
+   }
+
+}
